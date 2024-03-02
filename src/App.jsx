9096c6a40af9dc665d540654863bad97a7977491
olaide <olaideabiola2@gmail.com>
2024-03-02 18:35:22 +0100

    first assignment done

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <h1>STORY ABOUT MYSELF</h1>
+  <p>My name is Olaide Abiola Fuad, i am nigerian. i love technology alot and that is why i engage alot in the field. i started my career in tech as an hardware engineer and graphic designer. where i learnt basic troubleshooting techniques, formatting, partitioning and software installation and others. i learnt graphic designing with the use of coreldraw and photoshop. during my apprenticeship i wasn't too good at using this graphic design software or i would rather say my creative mind wasn't sharpen enough.
+  and this made me weiry alot to learn more so i started learning on my own by watching tutorials on what i've known and do not, i learnt the best design skills and tips even though i couldn't learn all of this in a year of my apprenticechip but i learnt continuously while studying in the higher institution because i gained admission after a year of completing my apprenticeship. i studied computer science, during my year of study in school learnt alot on my own with the help of online resources which helped me boost my skill in web development, i also joined the GDSC(google developer student club) and online bootcamp and today i am happy about my growth.
+  </p>
+    </>
+  )
+}
+
+export default App
